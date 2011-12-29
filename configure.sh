aae26ebb01e445b9849b039f359e7995e0d1bf61
Ruiyi Zhang <me@ruiyizhang.com>
2011-12-28 22:17:11 -0500

    added tmux configurations

--- configure.sh
+++ configure.sh
@@ -3,4 +3,5 @@ dotfile_path=$HOME/Developer/Projects/dotfilesx;
 ln -si $dotfile_path/bashrc ~/.bashrc
 ln -si $dotfile_path/hgrc ~/.hgrc
 ln -si $dotfile_path/gitconfig ~/.gitconfig
+ln -si $dotfile_path/tmux.conf ~/.tmux.conf
 ln -sin $dotfile_path/hgextensions ~/.hgextensions
