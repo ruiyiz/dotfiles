dab4c842bff04a3a768452cd59a6b4d64bb52f23
Ruiyi Zhang <me@ruiyizhang.com>
2011-12-28 22:04:43 -0500

    fixed cascading link of directories

--- configure.sh
+++ configure.sh
@@ -1,6 +1,6 @@
 dotfile_path=$HOME/Developer/Projects/dotfilesx;
 
-ln -sf $dotfile_path/bashrc ~/.bashrc
-ln -sf $dotfile_path/hgextensions ~/.hgextensions
-ln -sf $dotfile_path/hgrc ~/.hgrc
-ln -sf $dotfile_path/gitconfig ~/.gitconfig
+ln -si $dotfile_path/bashrc ~/.bashrc
+ln -si $dotfile_path/hgrc ~/.hgrc
+ln -si $dotfile_path/gitconfig ~/.gitconfig
+ln -sin $dotfile_path/hgextensions ~/.hgextensions
