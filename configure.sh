7d3a7003845005e0bca24f4f1249226fe7404ade
Ruiyi Zhang <me@ruiyizhang.com>
2015-02-07 23:24:56 -0500

    removed mercurial related files

--- configure.sh
+++ configure.sh
@@ -1,8 +1,6 @@
-dotfile_path=$HOME/Developer/Projects/dotfilesx;
+dotfile_path=$HOME/src/dotfilesx;
 
 ln -si $dotfile_path/bashrc ~/.bashrc
 ln -si $dotfile_path/bash_aliases ~/.bash_aliases
-ln -si $dotfile_path/hgrc ~/.hgrc
 ln -si $dotfile_path/gitconfig ~/.gitconfig
 ln -si $dotfile_path/tmux.conf ~/.tmux.conf
-ln -sin $dotfile_path/hgextensions ~/.hgextensions
