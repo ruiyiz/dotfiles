2270f7c47ca1f55358b1072e75a57b03fd1ebbae
Ruiyi Zhang <rzhang@1607capital.com>
2016-10-05 09:55:47 -0400

    per environment tmux status bar

--- configure.sh
+++ configure.sh
@@ -1,6 +1,6 @@
 dotfile_path=${PWD};
 
-ln -si $dotfile_path/bashrc ~/.bashrc
-ln -si $dotfile_path/bash_aliases ~/.bash_aliases
+ln -si $dotfile_path/zshrc ~/.zshrc
 ln -si $dotfile_path/gitconfig ~/.gitconfig
 ln -si $dotfile_path/tmux.conf ~/.tmux.conf
+ln -si $dotfile_path/tmux_prod.conf ~/.tmux_prod.conf
