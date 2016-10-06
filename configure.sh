5f57d403e6e613a6037ffeb218199b988ce465ec
Ruiyi Zhang <me@ruiyizhang.com>
2016-10-06 01:59:55 +0000

    removed production specific tmux conf file

--- configure.sh
+++ configure.sh
@@ -3,4 +3,3 @@ dotfile_path=${PWD};
 ln -si $dotfile_path/zshrc ~/.zshrc
 ln -si $dotfile_path/gitconfig ~/.gitconfig
 ln -si $dotfile_path/tmux.conf ~/.tmux.conf
-ln -si $dotfile_path/tmux_prod.conf ~/.tmux_prod.conf
