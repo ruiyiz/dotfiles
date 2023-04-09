362d387b0d0b68276bddc5ab050cd53871f6ecc4
Ruiyi Zhang <ruiyizhang@gmail.com>
2023-04-08 20:12:18 -0400

    yabai & skhd config

--- configure.sh
+++ configure.sh
@@ -1,5 +1,7 @@
 dotfile_path=${PWD};
 
-ln -si $dotfile_path/zshrc ~/.zshrc
+#ln -si $dotfile_path/zshrc ~/.zshrc
 ln -si $dotfile_path/gitconfig ~/.gitconfig
 ln -si $dotfile_path/tmux.conf ~/.tmux.conf
+ln -si $dotfile_path/yabairc ~/.yabairc
+ln -si $dotfile_path/skhdrc ~/.skhdrc
