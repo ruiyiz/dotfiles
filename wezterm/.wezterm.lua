1859106a0f64c58462b003b2a2e436ac13bd4277
Ruiyi Zhang <ruiyizhang@gmail.com>
2025-03-31 22:40:15 -0400

    wezterm font size

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,5 +1,5 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
@@ -9,28 +9,40 @@ local config = wezterm.config_builder()
 -- For example, changing the color scheme:
 -- config.color_scheme = 'AdventureTime'
 
--- Set font and font size
-config.font = wezterm.font('JetBrainsMono Nerd Font')
-config.font_size = 16
+-- Set font
+config.font = wezterm.font("JetBrainsMono Nerd Font")
+
+-- Set font size based on the operating system
+if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+	-- Windows: 14pt font size
+	config.font_size = 14
+elseif wezterm.target_triple == "x86_64-apple-darwin" or wezterm.target_triple == "aarch64-apple-darwin" then
+	-- macOS (Intel or Apple Silicon): 17pt font size
+	config.font_size = 17
+else
+	-- Default for other platforms
+	config.font_size = 16
+end
 
 -- Set initial window dimensions
 config.initial_cols = 120
 config.initial_rows = 60
 
 config.keys = {
-    -- Turn off the default CMD-m Hide action, allowing CMD-m to
-    -- be potentially recognized and handled by the tab
-    {
-      key = 'Enter',
-      mods = 'ALT',
-      action = wezterm.action.DisableDefaultAssignment,
-    },
+	-- Turn off the default CMD-m Hide action, allowing CMD-m to
+	-- be potentially recognized and handled by the tab
+	{
+		key = "Enter",
+		mods = "ALT",
+		action = wezterm.action.DisableDefaultAssignment,
+	},
 }
 
 -- Check if we're running on Windows and set WSL as default domain if so
-if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
-  config.default_domain = 'WSL:Ubuntu-24.04'
+if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+	config.default_domain = "WSL:Ubuntu-24.04"
 end
 
 -- and finally, return the configuration to wezterm
 return config
+
