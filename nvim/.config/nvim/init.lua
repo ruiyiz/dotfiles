05bc8031be0ddaf1836f268d39514fd351bfb887
Ruiyi Zhang <ruiyizhang@gmail.com>
2026-02-20 22:28:16 -0500

    feat(nvim): open neo-tree on startup when no file is specified

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,1 +1,12 @@
 require("config.lazy")
+
+-- Open Neo-tree when launching nvim without a file
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    if vim.fn.argc() == 0 then
+      vim.schedule(function()
+        vim.cmd("Neotree focus")
+      end)
+    end
+  end,
+})
